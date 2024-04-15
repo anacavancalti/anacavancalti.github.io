4f9f93a59a49d2bc1286b8f5f2746e70de40b748
Ana Cavalcanti <anacavancalti@gmail.com>
2024-04-15 16:00:56 +0200

    h1

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+<script>
+  var Name = prompt("Hoe heet je?"); if (name != null && name != ""){" "}
+  {(document.getElementById("welkom").innerHTML = "Hoi, " + Name + "!")}
+  else {(document.getElementById("welkom").innerHTML = "Welkom!")}
+</script>;
